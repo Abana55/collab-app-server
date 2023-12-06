175ae7e7653536b24765306bde7363ab0cc99c93
Alec bana <anbana994@gmail.com>
2023-12-05 19:53:00 -0500

    added routes for tables

---
+++ pool/pool.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+
+// Create a connection pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+// Export the pool for use in other files
+module.exports = pool.promise();
