bef5bd4e631a748c9fcf4135526e900153025395
Alec bana <anbana994@gmail.com>
2023-12-06 23:36:41 -0500

    added beggining fucntions

---
+++ controllers/artworkController.js
@@ -0,0 +1,21 @@
+const db = require('../utils/database');
+
+exports.getAllArtworks = async (req, res) => {
+    // Logic to fetch all artworks
+};
+
+exports.getArtworkById = async (req, res) => {
+    // Logic to fetch a specific artwork
+};
+
+exports.createArtwork = async (req, res) => {
+    // Logic to create a new artwork
+};
+
+exports.updateArtwork = async (req, res) => {
+    // Logic to update an artwork
+};
+
+exports.deleteArtwork = async (req, res) => {
+    // Logic to delete an artwork
+};
