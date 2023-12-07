c7389b049eb014e1b678af96c97634aeedd98c50
Alec bana <anbana994@gmail.com>
2023-12-06 23:37:43 -0500

    added collaboration controller

---
+++ controllers/collaborationController.js
@@ -0,0 +1,17 @@
+const db = require('../utils/database');
+
+exports.getAllCollaborations = async (req, res) => {
+    // Logic to fetch all collaborations
+};
+
+exports.getCollaborationById = async (req, res) => {
+    // Logic to fetch a specific collaboration
+};
+
+exports.createCollaboration = async (req, res) => {
+    // Logic to create a new collaboration
+};
+
+exports.deleteCollaboration = async (req, res) => {
+    // Logic to delete a collaboration
+};
