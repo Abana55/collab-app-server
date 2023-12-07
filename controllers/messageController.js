6e389f3bf1ef1b2dc0106393e7106263ae0a527b
Alec bana <anbana994@gmail.com>
2023-12-06 23:37:12 -0500

    controller fucntion for messages

---
+++ controllers/messageController.js
@@ -0,0 +1,9 @@
+const db = require('../utils/database');
+
+exports.getAllMessages = async (req, res) => {
+    // Logic to fetch all messages
+};
+
+exports.createMessage = async (req, res) => {
+    // Logic to create a new message
+};
