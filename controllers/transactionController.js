992096767feaa208b34779210621a746cd4cce48
Alec bana <anbana994@gmail.com>
2023-12-06 23:35:52 -0500

    transaction controller added

---
+++ controllers/transactionController.js
@@ -0,0 +1,9 @@
+const db = require('../utils/database');
+
+exports.getAllTransactions = async (req, res) => {
+    // Logic to fetch all transactions
+};
+
+exports.createTransaction = async (req, res) => {
+    // Logic to create a new transaction
+};
