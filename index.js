19fbc9701121af0862b495108b274d8bcf0ac2da
Alec bana <anbana994@gmail.com>
2023-12-11 22:48:20 -0500

    fixed routing

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const express = require('express');
 const cors = require('cors');
 const winston = require('winston');
 
-const express = require('express');
 const app = express();
+
+// Logger setup (Ensure this is correctly configured)
+const logger = winston.createLogger({
+    // logger configuration
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 // Import routes
 const userRoutes = require('./routes/users');
@@ -22,37 +30,27 @@ app.use('/api/messages', messageRoutes);
 app.use('/api/artworks', artworkRoutes);
 app.use('/api/transactions', transactionRoutes);
 
-// Other setup like database, middleware, etc.
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
 // Database setup (assuming you have a separate module for this)
 const db = require('./utils/database');
 
 // Verify database connection
 db.getConnection((err, connection) => {
-  if (err) {
-    logger.error('MySQL connection error:', err);
-    process.exit(1); // Exit if cannot connect to database
-  } else {
-    logger.info('Connected to MySQL');
-    connection.release();
-  }
+    if (err) {
+        logger.error('MySQL connection error:', err);
+        process.exit(1); // Exit if cannot connect to database
+    } else {
+        logger.info('Connected to MySQL');
+        connection.release();
+    }
 });
-
-// API Routes
-app.use('/api/users', userRoutes); // User routes
-// Add more routes here, e.g., app.use('/api/projects', projectRoutes);
 
 // Welcome route
 app.get('/', (req, res) => {
-  res.send('Welcome to the Collaboration App API');
+    res.send('Welcome to the Collaboration App API');
 });
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  logger.info(`Server is running on http://localhost:${PORT}`);
+    logger.info(`Server is running on http://localhost:${PORT}`);
 });
--- routes/collaborations.js
+++ routes/collaborations.js
@@ -1,4 +1,4 @@
-
+const express = require('express');
 const router = express.Router();
 const collaborationController = require('../controllers/collaborationController');
 
