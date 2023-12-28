f3234cb8981ab110d894f6266d0109748e2624bb
Alec bana <anbana994@gmail.com>
2023-12-27 19:50:07 -0500

    working on the auth login error messages

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const { generateToken } = require('../utils/jwtUtils');
+const Sequelize = require('sequelize'); // Import Sequelize to access error types
+
 
 
 const AuthController = {
@@ -28,29 +30,25 @@ const AuthController = {
     // User login
     async login(req, res) {
         try {
-            // Find the user by email
             const user = await User.findOne({ where: { email: req.body.email } });
             if (!user) {
                 return res.status(400).json({ message: 'User not found' });
             }
-
-            // Check if the password is correct
+    
             const isMatch = await bcrypt.compare(req.body.password, user.password);
             if (!isMatch) {
                 return res.status(400).json({ message: 'Invalid credentials' });
             }
-
-            // Create JWT token
-            const token = jwt.sign(
-                { userId: user.id }, // Use Sequelize model primary key field (id)
-                process.env.JWT_SECRET, // Ensure you have a JWT_SECRET in your .env file
-                { expiresIn: '24h' }
-            );
-            
-
+    
+            const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
             res.json({ token, userId: user.id });
         } catch (error) {
-            res.status(500).json({ error: error.message });
+            console.error(error); // Log the detailed error
+            if (error instanceof SomeSpecificError) { // Replace with specific error types if known
+                res.status(/* appropriate status code */).json({ message: /* specific error message */ });
+            } else {
+                res.status(500).json({ message: 'An internal server error occurred' });
+            }
         }
     }
 };
